8964d1b882699135fcfc80fa70b56a4581b18d54
Kaan Ozgokhan <kozgokhan@yahoo.com>
2019-05-26 22:30:11 +0200

    update

--- versioncheck.m
+++ versioncheck.m
@@ -1,25 +1,20 @@
 function versioncheck
-    
     fid = fopen('versionInfo.ldb');
     verInfo = textscan(fid,'%f %s','Delimiter',',');
     fclose(fid);
     
-    celldisp(verInfo)
+    currentVer = str2double(verInfo{2}{1}); lastUpdate = verInfo{1};
     
-    currentVer = verInfo{2}{1}; lastUpdate = verInfo{1};
-    
-    days = 10;
+    days = 100;
     if (now-lastUpdate)/(6*60*24) > days
         options = weboptions('ContentType','text');
         latestVersion = webread('https://raw.githubusercontent.com/kozgokhan/avlie/master/latestVersion',options);
-        if strcmp(latestVersion(strfind(latestVersion,':')+2:strfind(latestVersion,':')+4),currentVer)
+        if str2double(latestVersion(strfind(latestVersion,':')+2:strfind(latestVersion,':')+4)) <= currentVer
             disp('version guncel');
         else
             disp('guncelleme gerekli');
         end
     else
-        disp(['sonraki guncelleme kontrolune: ', num2str(days-(now-lastUpdate)/(6*60*24)), ' gun kaldi.'])
+        %disp(['sonraki guncelleme kontrolune: ', num2str(days-(now-lastUpdate)/(6*60*24)), ' gun kaldi.'])
     end
-    
-    
 end
